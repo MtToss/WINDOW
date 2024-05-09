951e29b77408010ba960c2f94006e7e66e345a46
MtToss <cherrypogiced@gmail.com>
2024-05-10 02:00:01 +0800

    deleted unnecessary files and created a function in the TitleBarController.java which I will be working on mamaya or Drei will push it through

--- src/main/java/buginvaderz/window/TitleBarController.java
+++ src/main/java/buginvaderz/window/TitleBarController.java
@@ -5,19 +5,17 @@ import javafx.scene.layout.BorderPane;
 
 public class TitleBarController {
 
-    private BorderPane borderPane; // Reference to the BorderPane
 
 
 
     @FXML
     public void minimizeButton() {
         System.out.println("HAI");
-        // Minimize the borderPane (You can implement your logic here)
-        borderPane.setVisible(false); // Example: hiding the BorderPane
+
     }
 
     @FXML
     public void closeButton() {
-        // Implement your close button logic here
+
     }
 }
